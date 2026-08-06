c52ba5da385bfbab6f3364dff9539ca3ce86d607
RAJSH1612 <rajshekhar16062006@gmail.com>
2026-08-06 23:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.22%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -6,7 +6,8 @@ public:
 
        
         for (int i = 0; i <= n - m; i++) {
-            int j = 0;
+             int first=i;
+             int j=0;
 
             
             while (j < m && haystack[i + j] == needle[j]) {

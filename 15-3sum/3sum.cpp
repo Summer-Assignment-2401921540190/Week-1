c556fd37ea0eacb6bd6f3ceb924c3bb1d254474a
RAJSH1612 <rajshekhar16062006@gmail.com>
2026-08-08 23:55:00 +0530

    Time: 51 ms (42.73%) | Memory: 29 MB (73.59%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,50 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-        vector<vector<int>> ans;
-
+        vector<vector<int>> output;
+        
+        // 1. Sort the array to use the two-pointer technique
         sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < nums.size() - 2; i++) {
-
-            if(i > 0 && nums[i] == nums[i-1])
-                continue;
-
-            int st = i + 1;
-            int end = nums.size() - 1;
-
-            while(st < end) {
-
-                int sum = nums[i] + nums[st] + nums[end];
-
-                if(sum == 0) {
-
-                    ans.push_back({nums[i], nums[st], nums[end]});
-
-                    st++;
-                    end--;
-
-                    while(st < end && nums[st] == nums[st-1])
-                        st++;
-
-                    while(st < end && nums[end] == nums[end+1])
-                        end--;
-                }
-                else if(sum < 0) {
-                    st++;
-                }
-                else {
-                    end--;
+        
+        // 2. Iterate through the array for the first element
+        for (int i = 0; i < nums.size(); i++) {
+            // Skip duplicate values for the first element to avoid duplicate triplets
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            // Two pointers initialization
+            int j = i + 1;
+            int k = nums.size() - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum == 0) {
+                    // Found a valid triplet
+                    output.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    
+                    // Skip duplicate values for the second element
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    // Skip duplicate values for the third element
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                    
+                } else if (sum < 0) {
+                    // Sum is too small, move the left pointer rightward to increase sum
+                    j++;
+                } else {
+                    // Sum is too large, move the right pointer leftward to decrease sum
+                    k--;
                 }
             }
         }
-
-        return ans;
+        
+        // The missing fix from the image code
+        return output; 
     }
 };
